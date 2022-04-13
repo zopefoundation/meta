3a251aab94eff9cf2aa2fd87d7266593251e4b76
Michael Howitz <mh@gocept.com>
2022-04-13 07:45:42 +0200

    Update future Python to 3.11.0-alpha.7.

--- config/config-package.py
+++ config/config-package.py
@@ -19,7 +19,7 @@ META_HINT_MARKDOWN = """\
 Generated from:
 https://github.com/zopefoundation/meta/tree/master/config/{config_type}
 --> """
-FUTUTRE_PYTHON_VERSION = "3.11.0-alpha.6"
+FUTUTRE_PYTHON_VERSION = "3.11.0-alpha.7"
 
 
 def copy_with_meta(
