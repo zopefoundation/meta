28abbfda464ffc9595d0b7d61d8714da1ccd7056
Michael Howitz <icemac@gmx.net>
2023-08-29 08:33:12 +0200

    Update future python to current version.

--- config/config-package.py
+++ config/config-package.py
@@ -23,7 +23,7 @@ META_HINT_MARKDOWN = """\
 Generated from:
 https://github.com/zopefoundation/meta/tree/master/config/{config_type}
 --> """
-FUTURE_PYTHON_VERSION = "3.12.0-beta.4"
+FUTURE_PYTHON_VERSION = "3.12.0-rc.1"
 DEFAULT = object()
 
 
