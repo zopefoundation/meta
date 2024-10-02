810789ca39bd70963411fe25c909c969adfc3368
Jens Vagelpohl <jens@plyp.com>
2024-10-02 10:51:53 +0200

    - prepare release 1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Change log
 ==========
 
-1.0 (unreleased)
+1.0 (2024-10-02)
 ----------------
 
 - Converted to an installable Python package.
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 setup(
     name='zope.meta',
-    version='1.0.dev0',
+    version='1.0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description='Helper functions for package management',
