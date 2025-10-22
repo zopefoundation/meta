9d51a543d6b9d1846994cba7e7d298fc5b946e0b
Michael Howitz <icemac@gmx.net>
2025-10-22 09:02:51 +0200

    Update code to Python 3.10+

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         'License :: OSI Approved :: Zope Public License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
@@ -70,7 +69,7 @@ setup(
         'tox',
         'zest.releaser',
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     include_package_data=True,
     zip_safe=False,
     extras_require={
