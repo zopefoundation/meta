d4b7f3ba059ee78cef1c90f1b155d4d2c959ec2c
Michael Howitz <icemac@gmx.net>
2025-10-28 08:23:23 +0100

    Back to development: 2.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
 
 setup(
     name='zope.meta',
-    version='2.0',
+    version='2.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description='Helper functions for package management',
